eebab635ed652365ffb18c6d4a135fa8eca79d1c
clara <clara@minneapolis.edu>
2019-11-12 18:54:32 -0600

    quit

--- src/main/java/week_10/Main.java
+++ src/main/java/week_10/Main.java
@@ -26,7 +26,5 @@ public class Main {
     public static void quit(List<String> wishList, List<String> visitedList) {
         storage.writeListToFile(wishList, wishListFile);
         storage.writeListToFile(visitedList, visitedFile);
-        
-        System.exit(0);    // Ends the program
     }
 }
